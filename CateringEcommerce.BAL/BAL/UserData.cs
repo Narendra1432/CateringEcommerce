502b9296968fd40c7e95fea5687b4244da9d381c
Narendra1432 <narendrapandey2001@gmail.com>
2025-04-19 04:46:07 -0700

    Initial commit related to Model and Controller

---
+++ CateringEcommerce.BAL/BAL/UserData.cs
@@ -0,0 +1,31 @@
+using CateringEcommerce.Domain;
+using CateringEcommerce.Domain.Models;
+
+namespace CateringEcommerce.BAL
+{
+    public class UserData
+    {
+        public List<AdminModel> GetAllUser()
+        {
+
+        }
+
+        public List<AdminModel> GetUserById(int id)
+        {
+
+        }
+
+        public bool DeleteUser(int id)
+        {
+
+        }
+        public bool InsertUserDetails(UserModel userModel)
+        {
+
+        }
+        public bool UpdateUserDetails(int id ,UserModel userModel)
+        {
+
+        }
+    }
+}
