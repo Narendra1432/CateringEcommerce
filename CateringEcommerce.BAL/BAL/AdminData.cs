64e677e7588fef85613ff4bef7f083f18c45ce99
Narendra1432 <narendrapandey2001@gmail.com>
2025-04-19 04:46:07 -0700

    In the Ecommmerce sub branch 2 commit related to Model and Controller

---
+++ CateringEcommerce.BAL/BAL/AdminData.cs
@@ -0,0 +1,31 @@
+using CateringEcommerce.Domain;
+using CateringEcommerce.Domain.Models;
+
+namespace CateringEcommerce.BAL
+{
+    public class AdminData
+    {
+        public List<AdminModel> GetAllAdmin()
+        {
+
+        }
+
+        public List<AdminModel> GetAdminById(int id)
+        {
+
+        }
+
+        public bool DeleteAdmin(int id)
+        {
+
+        }
+        public bool InsertAdminDetails(AdminModel adminModel)
+        {
+
+        }
+        public bool UpdateAdminDetails(int id ,AdminModel adminModel)
+        {
+
+        }
+    }
+}
